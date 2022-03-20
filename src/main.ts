92ebc6ab9426ce88f4d6d15b4deabca124010ae6
mintnguyen.pa <mintnguyen.pa@gmail.com>
2022-03-20 14:27:42 -0700

    change port

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+const PORT = 4000;
 async function bootstrap() {
+  const logger = new Logger();
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  logger.log(`Application listening on port ${PORT}`);
+  logger.log('http://localhost:' + PORT + '/graphql');
+  await app.listen(PORT);
 }
 bootstrap();
